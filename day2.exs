9cc3812cfc266c864df7fc6761095348fdaa0024
Monica Giambitto <monica.giambitto@freeletics.com>
2017-12-02 21:18:28 +0100

    day 2 - take 2, evenly divisible

--- day2.exs
+++ day2.exs
@@ -3,7 +3,8 @@ defmodule AdventOfCode do
     input
     |> String.split(["\n"], trim: true)
     |> to_list_of_lists
-    |> Enum.map(fn(x) -> min_max_difference x end)
+    |> Enum.map(fn(x) -> Enum.sort(x, &(&1 >= &2)) end)
+    |> Enum.map(fn(x) -> _division_of_evenly x end)
     |> Enum.sum
   end
 
@@ -18,11 +19,13 @@ defmodule AdventOfCode do
     |> Enum.map(&String.to_integer/1)
   end
 
-  defp min_max_difference(input) do
-    x =
-    input
-    |> Enum.min_max
-
-    elem(x, 1) - elem(x, 0)
+  defp _division_of_evenly([]), do: 0
+  defp _division_of_evenly([head | tail]) do
+    case evenly_divisible(head, tail) do
+      [] -> _division_of_evenly(tail)
+      x -> round(head / hd(x))
+    end
   end
+
+  defp evenly_divisible(element, list), do: Enum.filter(list, fn(x) -> rem(element, x) == 0 end)
 end
--- test/day2.exs
+++ test/day2.exs
@@ -3,17 +3,17 @@ ExUnit.start
 defmodule AdventOfCodeTest do
   use ExUnit.Case
 
-  test "checksum is the sum of differences of min and max of all lines"do
+  test "checksum is the sum of the division of the two evenly divisible numbers on all lines"do
     input =
 """
-5 1 9 5
-7 5 3
-2 4 6 8
+5 9 2 8
+9 4 7 3
+3 8 6 5
 """
-    assert AdventOfCode.checksum(input) == 18
+    assert AdventOfCode.checksum(input) == 9
   end
 
-  test "checksum of single line is the difference of min and max of the line" do
-    assert AdventOfCode.checksum("5 1 9 5") == 8
+  test "checksum of single line is the division of the two evenly divisible numbers of the line" do
+    assert AdventOfCode.checksum("5 9 2 8") == 4
   end
 end
