806c62a6a91b2d98c26ebe4ac90f4163504529b5
Monica Giambitto <monica.giambitto@freeletics.com>
2017-12-02 03:29:03 +0100

    day 1 - take two

--- day1.exs
+++ day1.exs
@@ -1,16 +1,22 @@
 defmodule AdventOfCode do
   def inverse_captcha(input) do
-    input_list = to_list(input)
+    inputs_list = to_lists(input)
 
-    input_list
-    |> _inverse_captcha
-    |> Kernel.+(_inverse_captcha([List.last(input_list), List.first(input_list)]))
+    inputs_list
+    |> List.first
+    |> _inverse_captcha(List.last(inputs_list))
   end
 
-  defp to_list(input) do
+  defp to_lists(input) do
     input
     |> String.graphemes
     |> Enum.map(&String.to_integer/1)
+    |> Enum.split(captcha_length(input))
+    |> Tuple.to_list
+  end
+
+  defp captcha_length(input) do
+    round(String.length(input) / 2)
   end
 
   defp _inverse_captcha([]), do: 0
@@ -19,5 +25,13 @@ defmodule AdventOfCode do
   end
   defp _inverse_captcha([ _ | tail]) do
     _inverse_captcha(tail)
+  end
+
+  defp _inverse_captcha([], []), do: 0
+  defp _inverse_captcha([first_head | first_tail], [second_head | second_tail]) when first_head == second_head do
+    first_head + second_head + _inverse_captcha(first_tail, second_tail)
+  end
+  defp _inverse_captcha([ _ | first_tail], [ _ | second_tail]) do
+    _inverse_captcha(first_tail, second_tail)
   end
 end
