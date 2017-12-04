a02e01ec9fa86935c5e422b5c4ae05b2d547faa3
Monica Giambitto <monica.giambitto@freeletics.com>
2017-12-04 16:16:48 +0100

    day 4 - number of valid passphrases

--- 2017/day4.exs
+++ 2017/day4.exs
@@ -1,16 +1,35 @@
 defmodule AdventOfCode do
-  def valid?(input) do
+  def valid_passphrases(input) do
+    # passphrases = passphrases(input)
+    input
+    |> passphrases
+    |> Enum.map(fn(p) -> unique?(p) end)
+    |> Enum.reject(fn(u) -> not u end)
+    |> Enum.count
+  end
+
+  defp passphrases(input) do
+    input
+    |> String.split("\n", trim: true)
+  end
+
+  def words(input) do
     input
     |> String.split
-    |> duplicate_in?
+  end
+
+  def unique?(input) do
+    input
+    |> words
+    |> duplicate_word?
     |> Kernel.not
   end
 
-  defp duplicate_in?([]), do: false
-  defp duplicate_in?([head | tail]) do
+  def duplicate_word?([]), do: false
+  def duplicate_word?([head | tail]) do
     case Enum.any?(tail, fn(x) -> x == head end) do
       true -> true
-      false -> duplicate_in?(tail)
+      false -> duplicate_word?(tail)
     end
   end
 end
--- 2017/test/day4.exs
+++ 2017/test/day4.exs
@@ -3,22 +3,28 @@ ExUnit.start
 defmodule AdventOfCodeTest do
   use ExUnit.Case
 
-  test "passphrase with unique words are valid" do
-    assert AdventOfCode.valid?("a") == true
-    assert AdventOfCode.valid?("a aa") == true
-    assert AdventOfCode.valid?("a\naa") == true
-    assert AdventOfCode.valid?("aa bb cc dd ee") == true
-    assert AdventOfCode.valid?("aa bb cc dd aaa") == true
+  test "passphrase with unique words are counted" do
+    assert AdventOfCode.valid_passphrases("a") == 1
+    assert AdventOfCode.valid_passphrases("a aa") == 1
+    assert AdventOfCode.valid_passphrases("a\naa") == 2
+    assert AdventOfCode.valid_passphrases("aa bb cc dd ee") == 1
+    assert AdventOfCode.valid_passphrases("aa bb cc dd aaa") == 1
   end
 
-  test "passphrase with duplicated words are not valid" do
-    assert AdventOfCode.valid?("a a") == false
-    assert AdventOfCode.valid?("a\na") == false
-    assert AdventOfCode.valid?("aa bb cc dd aa") == false
-    assert AdventOfCode.valid?("aa bb bb dd ee") == false
-    assert AdventOfCode.valid?("""
-      aa bb cc
-      dd aa
-    """) == false
+  test "passphrase with duplicated words are not counted" do
+    assert AdventOfCode.valid_passphrases("a a") == 0
+    assert AdventOfCode.valid_passphrases("a\na") == 2
+    assert AdventOfCode.valid_passphrases("aa bb cc dd aa") == 0
+    assert AdventOfCode.valid_passphrases("aa bb bb dd ee") == 0
+  end
+
+  test "valid passphrases count only unique passphrases" do
+    assert AdventOfCode.valid_passphrases(
+      """
+      sayndz zfxlkl attjtww cti sokkmty brx fhh suelqbp
+      xmuf znkhaes pggrlp zia znkhaes znkhaes
+      nti rxr bogebb zdwrin
+      sryookh unrudn zrkz jxhrdo gctlyz
+      """) == 3
   end
 end
