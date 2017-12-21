8845e17b9839526e816659f11b3c67b2d0760cde
Monica Giambitto <monica.giambitto@freeletics.com>
2017-12-21 17:15:42 +0100

    day 7 - add parsing of input

--- 2017/test/day7.exs
+++ 2017/test/day7.exs
@@ -5,21 +5,22 @@ defmodule Day7Test do
 
   describe "AdventOfCode.root/1" do
     test "returns the root program" do
-      input = [
-        ["pbga", 66, []],
-        ["xhth", 57, []],
-        ["ebii", 61, []],
-        ["havc", 66, []],
-        ["ktlj", 57, []],
-        ["fwft", 72, ["ktlj", "cntj", "xhth"]],
-        ["qoyq", 66, []],
-        ["padx", 45, ["pbga", "havc", "qoyq"]],
-        ["tknk", 41, ["ugml", "padx", "fwft"]],
-        ["jptl", 61, []],
-        ["ugml", 68, ["gyxo", "ebii", "jptl"]],
-        ["gyxo", 61, []],
-        ["cntj", 57, []]
-      ]
+      input =
+"""
+pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)
+"""
 
       assert Day7.root(input) == "tknk"
     end
