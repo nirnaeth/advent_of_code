f279d4bb76a5445e0c26cad7b1c98973da244ec0
Monica Giambitto <monica.giambitto@freeletics.com>
2018-12-06 15:33:44 +0100

    partially working solution for day1/2

--- 2018/frequency_analyzer.exs
+++ 2018/frequency_analyzer.exs
@@ -5,21 +5,30 @@ defmodule FrequencyAnalyzer do
     |> Enum.sum
   end
 
+  def calibration_frequency(input) do
+    input
+    |> to_integers
+    |> Enum.map_reduce(0, fn x, acc -> {acc, x + acc} end)
+    |> calibrate(input |> to_integers)
+  end
+
   defp to_integers(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&String.to_integer/1)
   end
 
-#  recursive function?
-  def calibration_frequency(input) do
-    input
-    |> to_integers
-    |> Enum.map_reduce(0, fn x, acc -> {acc, x + acc} end)
+  def calibrate({frequencies, frequency}, input) do
+    case calibrated?([frequency | frequencies]) do
+      true -> frequency
+      false -> input
+               |> Enum.map_reduce(frequency, fn x, acc -> {acc, x + acc} end)
+               |> calibrate(input)
+    end
   end
 
-  def calibrated?([]), do: false
-  def calibrated?([head | tail]) do
+  defp calibrated?([]), do: false
+  defp calibrated?([head | tail]) do
     case Enum.any?(tail, fn(x) -> x == head end) do
       true -> true
       false -> calibrated?(tail)
