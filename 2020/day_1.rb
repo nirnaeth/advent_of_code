6f4a3258ef392beec77e66564168ec4fb07cce2d
Monica Giambitto <monica.giambitto@gmail.com>
2020-12-06 21:33:44 +0100

    day 1 rename method

--- 2020/day_1.rb
+++ 2020/day_1.rb
@@ -4,7 +4,7 @@ require "input.rb"
 path = "#{File.expand_path File.dirname(__FILE__)}/support/input_day_1.txt"
 source = Input.new(path).to_array
 
-def adding_to_2020(source)
+def add_two_to_2020(source)
   to_2020 = source.map { |n| 2020 - n }
   source & to_2020
 end
@@ -16,6 +16,6 @@ def add_three_to_2020(source)
     pop
 end
 
-# adding_to_2020(source).reduce(:*)
+add_two_to_2020(source).reduce(:*)
 
 add_three_to_2020(source).reduce(:*)
