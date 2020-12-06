d62293b9831c4fba1265b036264309fe9117be38
Monica Giambitto <monica.giambitto@gmail.com>
2020-12-06 21:52:01 +0100

    day 1 rename input spec file

--- 2020/spec/day_1_spec.rb
+++ 2020/spec/day_1_spec.rb
@@ -3,7 +3,7 @@ require "input.rb"
 
 RSpec.describe "#add_two_to_2020" do
   it "returns two numbers adding to 2020" do
-    path = "#{File.expand_path File.dirname(__FILE__)}/support/input.txt"
+    path = "#{File.expand_path File.dirname(__FILE__)}/support/input_day_1_spec.txt"
     source = Input.new(path).to_array
 
     expect(add_two_to_2020(source)).to eq [1721, 299]
@@ -12,7 +12,7 @@ end
 
 RSpec.describe "#add_three_to_2020" do
   it "returns three numbers adding to 2020" do
-    path = "#{File.expand_path File.dirname(__FILE__)}/support/input.txt"
+    path = "#{File.expand_path File.dirname(__FILE__)}/support/input_day_1_spec.txt"
     source = Input.new(path).to_array
 
     expect(add_three_to_2020(source)).to eq [979, 366, 675]
