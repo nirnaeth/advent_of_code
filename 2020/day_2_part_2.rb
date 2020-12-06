6e4403066f189ddcdf69dda85c027a4b36229d76
Monica Giambitto <monica.giambitto@gmail.com>
2020-12-06 23:34:19 +0100

    day 2 part 2

---
+++ 2020/day_2_part_2.rb
@@ -0,0 +1,27 @@
+$LOAD_PATH << './lib'
+require "input.rb"
+
+path = "#{File.expand_path File.dirname(__FILE__)}/support/input_day_2.txt"
+source = Input.new(path).to_str_array
+
+def number_of_valid_passwords(source)
+  valid_passwords(source).count
+end
+
+def valid_passwords(source)
+  source.select { |item| valid?(item) }
+end
+
+def valid?(item)
+  position_one, position_two, letter, password = separate(item)
+
+  (password[position_one - 1] + password[position_two - 1]).count(letter) == 1
+end
+
+def separate(item)
+  matches = /(?<one>\d+)-(?<two>\d+) (?<char>.): (?<password>[a-z]+)/.match(item)
+
+  return matches["one"].to_i, matches["two"].to_i, matches["char"], matches["password"]
+end
+
+p number_of_valid_passwords(source)
