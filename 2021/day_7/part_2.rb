a14e8d5af6c51ea3f54b9f7b40682d6e36d91cd7
Monica Giambitto <m.giambitto@freegoweb.it>
2021-12-08 00:26:22 +0100

    2021 day 7 part 2 - wrong solutions only

--- 2021/day_7/part_2.rb
+++ 2021/day_7/part_2.rb
@@ -7,31 +7,116 @@ require 'pry'
 path = 'support/day_7/input.txt'
 crabs = Input.new(path).to_int_array(',')
 
+# -------------- BRUTE FORCE #1
+# def find_distances(crabs)
+#   max_distance = crabs.max + 1
+
+#   [].tap do |distances|
+#     (0..max_distance).each do |target|
+#       distances[target] = []
+
+#       crabs.each do |position|
+#         distance = position - target
+#         distances[target] << distance
+#       end
+#     end
+#   end
+# end
+
+# distances = find_distances(crabs)
+
+# def min_distance(distances)
+#   steps = []
+#   distances.each do |distance|
+#     steps << distance.sum
+#   end
+
+#   min = 0
+
+#   [].tap do |absolute_distances|
+#     steps.each do |current|
+#       absolute_distances << (0 - current).abs
+#     end
+
+#     min = absolute_distances.min
+#   end.find_index(min)
+# end
+
+# min = min_distance(distances)
+
+# def calculate_fuel(steps)
+#   fuel = 0
+  
+#   steps.each do |n|
+#     n = n.abs
+#     fuel += (n * (n + 1)) / 2
+#   end
+
+#   fuel
+# end
+
+# calculate_fuel(distances[min])
+
+# -------------- BRUTE FORCE #2
+# def calculate_fuel(crabs)
+#   [].tap do |consumptions|
+#     (0..crabs.max).each do |n|
+#       [].tap do |fuels|
+#         crabs.each do |crab|
+#           distance = (crab - n)
+#           # fuels << gaussian_sum(distance)
+#           fuels << distance
+#         end
+#         consumptions << fuels
+#       end
+#     end
+#     # binding.pry
+    
+#     [].tap do |test|
+#       consumptions.each do |c|
+#         test << c.sum
+#       end
+
+#       binding.pry
+      
+#       [].tap do |values|
+#         test.min.each do |c|
+#           gaussian_sum(c)
+#         end
+
+#         values.sum
+#       end
+#     end
+#   end
+# end
+
+# calculate_fuel(crabs)
+
+# --------- MATH SOLUTION
 # https://www.reddit.com/r/adventofcode/comments/rar7ty/2021_day_7_solutions/hnkd58g/?context=3
-def blast_position(crabs)
-  crabs.sum.to_f / crabs.size.to_f
-end
+# def blast_position(crabs)
+#   (crabs.sum.to_f / crabs.size.to_f)
+# end
 
-position = blast_position(crabs)
+# position = blast_position(crabs)
 
-p position
+# def gaussian_sum(n)
+#   (n * (n + 1)) / 2
+# end
 
-def gaussian_sum(n)
-  (n * (n + 1)) / 2
-end
+# def calculate_fuel(crabs, position)
+#   [].tap do |distances|
+#     crabs.each do |crab|
+#       distance = (crab - position).abs
+#       distance = if (crab - position) > 0
+#         distance.floor
+#       else
+#         distance.ceil
+#       end
+#       
+#       distances << gaussian_sum(distance)
+#     end
+#   end.sum
+# end
 
-def calculate_fuel(crabs, position)
-  [].tap do |distances|
-    crabs.each do |crab|
-
-      floor = (crab - position.floor).abs
-      ceil = (crab - position.ceil).abs
-
-      # have to account for 0.5 variance
-      distances << gaussian_sum(position - position.floor > position - position.ceil ? ceil : floor)
-    end
-  end.sum
-
-end
-
-p calculate_fuel(crabs, position)
+# p calculate_fuel(crabs, position)
--- 2021/spec/day_7/part_2_spec.rb
+++ 2021/spec/day_7/part_2_spec.rb
@@ -1,23 +1,79 @@
 require './day_7/part_2'
 require 'pry'
 
-RSpec.describe '#blast_position' do
-  it 'returns the location that is the least expensive for fuel' do
+#16,1,2,0,4,2,7,1,2,14
+RSpec.describe '#find_distances' do
+  it 'calculates all possible distances for all possible crabs' do
     path = 'spec/support/day_7/input.txt'
     crabs = Input.new(path).to_int_array(',')
 
-    expect(blast_position(crabs)).to eq 4.9
+    distances = find_distances(crabs)
+    
+    expect(distances).to eq expected
+  end  
+end
+
+RSpec.describe '#min_distance' do
+  it 'finds the shortest distance all crabs have to move' do
+    distances = expected
+
+    expect(min_distance(distances)).to eq 5
   end
 end
 
-RSpec.describe 'calculate_fuel' do
-  it 'returns the total amount of fuel needed to move the crabs' do
+RSpec.describe '#calculate_fuel' do
+  it 'returns the total fuel needed to move the crabs' do
     path = 'spec/support/day_7/input.txt'
     crabs = Input.new(path).to_int_array(',')
-    position = 4.9
 
-    fuel = calculate_fuel(crabs, position)
+    distances = find_distances(crabs)
+
+    fuel = calculate_fuel(distances[5])
+
     expect(fuel).to eq 168
   end
 end
+# RSpec.describe '#blast_position' do
+#   it 'returns the location that is the least expensive for fuel' do
+#     path = 'spec/support/day_7/input.txt'
+#     crabs = Input.new(path).to_int_array(',')
 
+#     expect(blast_position(crabs)).to eq 4.9
+#   end
+# end
+
+# RSpec.describe 'calculate_fuel' do
+#   it 'returns the total amount of fuel needed to move the crabs' do
+#     path = 'spec/support/day_7/input.txt'
+#     crabs = Input.new(path).to_int_array(',')
+#     position = 4.9
+
+#     fuel = calculate_fuel(crabs, position)
+    
+#     expect(fuel).to eq 168
+#   end
+# end
+
+def expected
+  [
+    [16, 1, 2, 0, 4, 2, 7, 1, 2, 14], 
+    [15, 0, 1, -1, 3, 1, 6, 0, 1, 13], 
+    [14, -1, 0, -2, 2, 0, 5, -1, 0, 12], 
+    [13, -2, -1, -3, 1, -1, 4, -2, -1, 11], 
+    [12, -3, -2, -4, 0, -2, 3, -3, -2, 10], 
+    [11, -4, -3, -5, -1, -3, 2, -4, -3, 9], 
+    [10, -5, -4, -6, -2, -4, 1, -5, -4, 8], 
+    [9, -6, -5, -7, -3, -5, 0, -6, -5, 7], 
+    [8, -7, -6, -8, -4, -6, -1, -7, -6, 6], 
+    [7, -8, -7, -9, -5, -7, -2, -8, -7, 5], 
+    [6, -9, -8, -10, -6, -8, -3, -9, -8, 4], 
+    [5, -10, -9, -11, -7, -9, -4, -10, -9, 3], 
+    [4, -11, -10, -12, -8, -10, -5, -11, -10, 2], 
+    [3, -12, -11, -13, -9, -11, -6, -12, -11, 1], 
+    [2, -13, -12, -14, -10, -12, -7, -13, -12, 0], 
+    [1, -14, -13, -15, -11, -13, -8, -14, -13, -1], 
+    [0, -15, -14, -16, -12, -14, -9, -15, -14, -2], 
+    [-1, -16, -15, -17, -13, -15, -10, -16, -15, -3]
+  ]
+  
+end
