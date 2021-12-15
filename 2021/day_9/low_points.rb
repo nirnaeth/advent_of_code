add213741eed687a0def10f8cedd75a55a14752c
Monica Giambitto <m.giambitto@freegoweb.it>
2021-12-15 12:14:08 +0100

    2021 day 9 part 2 - add coordinates to low points and neighbours

--- 2021/day_9/low_points.rb
+++ 2021/day_9/low_points.rb
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2021/day/8
+# https://adventofcode.com/2021/day/9
 
 $LOAD_PATH << './lib'
 require 'input'
@@ -8,7 +8,8 @@ heightmap = Input.new(path).to_bit_matrix
 
 Coordinate = Struct.new(:x, :y)
 
-def neighbours(coordinates, map)
+# ----- part 1
+def neighbours_values(coordinates, map)
   neighbours = []
 
   neighbours << map[coordinates.y][coordinates.x - 1] if coordinates.x > 0 # right
@@ -19,11 +20,11 @@ def neighbours(coordinates, map)
   neighbours
 end
 
-def low_points(map)
+def low_points_values(map)
   [].tap do |result|
     map.each_with_index do |row, i|
       row.each_with_index do |element, j|
-        result << element if element < neighbours(Coordinate.new(j, i), map).min
+        result << element if element < neighbours_values(Coordinate.new(j, i), map).min
       end
     end
   end
@@ -33,6 +34,72 @@ def risk(points)
   points.inject(0) { |sum, n| sum + (n + 1) } 
 end
 
-points = low_points(heightmap)
+points = low_points_values(heightmap)
 
-p risk(points)
+risk(points)
+
+# ----- part 2
+
+# Uses flood fill algorithm 
+# https://www.reddit.com/r/adventofcode/comments/rcfveb/2021_day_9_on_realising_these_things_are_the_same/hnwa2hs/
+# approach 2 described here 
+# https://www.reddit.com/r/adventofcode/comments/rcfveb/2021_day_9_on_realising_these_things_are_the_same/hnvn6nj/ 
+# and marking the visited nodes as a 9
+# other options are BfS https://algotree.org/algorithms/tree_graph_traversal/breadth_first_search/
+
+# each point is represented by the coordinates as key y:x or row:column and the height as the value
+def low_points(map)
+  {}.tap do |result|
+    map.each_with_index do |row, i|
+      row.each_with_index do |element, j|
+        result["#{i}:#{j}"] = element if element < neighbours_values(Coordinate.new(j, i), map).min
+      end
+    end
+  end
+end
+
+low_points = low_points(heightmap)
+
+def neighbours(coordinates, map)
+  neighbours = {}
+
+  neighbours["#{coordinates.y}:#{coordinates.x - 1}"] = map[coordinates.y][coordinates.x - 1] if coordinates.x > 0 # right
+  neighbours["#{coordinates.y + 1}:#{coordinates.x}"] = map[coordinates.y + 1][coordinates.x] if coordinates.y + 1 < map.size # down
+  neighbours["#{coordinates.y}:#{coordinates.x + 1}"] = map[coordinates.y][coordinates.x + 1] if coordinates.x + 1 < map.first.size # left
+  neighbours["#{coordinates.y - 1}:#{coordinates.x}"] = map[coordinates.y - 1][coordinates.x] if coordinates.y > 0 # top
+  
+  neighbours
+end
+
+# Flood-fill (node):
+#   1. Set Q to the empty queue or stack.
+#   2. Add node to the end of Q.
+#   3. While Q is not empty:
+#   4.   Set n equal to the first element of Q.
+#   5.   Remove first element from Q.
+#   6.   If n is Inside:
+#          Set the n
+#          Add the node to the west of n to the end of Q.
+#          Add the node to the east of n to the end of Q.
+#          Add the node to the north of n to the end of Q.
+#          Add the node to the south of n to the end of Q.
+#   7. Continue looping until Q is exhausted.
+#   8. Return.
+
+# stack = {}
+
+# low_points.each do |point|
+#   stack.merge point
+  
+#   while stack.any? do
+#     n = stack.pop
+
+#     if n.value != 9 # n is "inside"
+#       stack[n.value] = 9
+#       y = n.key.split(':').first
+#       x = n.key.split(':').last
+#       stack.merge neighbours(Coordinate.new(y, x), heightmap)
+#     end
+#   end
+# end
+
--- 2021/spec/day_9/part_1_spec.rb
+++ 2021/spec/day_9/part_1_spec.rb
@@ -1,29 +1,59 @@
 require './day_9/low_points'
 require 'pry'
 
-RSpec.describe '#neighbours' do
+RSpec.describe '#neighbours_values' do
   it 'finds the neighbours of the specified location' do
-    input_map = map
     coordinates = Coordinate.new(2, 3)
     
-    expect(neighbours(coordinates, input_map)).to eq [7, 9, 7, 5]
+    expect(neighbours_values(coordinates, map)).to eq [7, 9, 7, 5]
   end
 
   it 'returns less than 4 neighbours if the element is on the border of the map' do
-    input_map = map
     top_left = Coordinate.new(0, 0)
     bottom_right = Coordinate.new(map.first.size - 1, map.size - 1)
 
-    expect(neighbours(top_left, input_map)).to match_array [1, 3]
-    expect(neighbours(bottom_right, input_map)).to match_array [9, 7]
+    expect(neighbours_values(top_left, map)).to match_array [1, 3]
+    expect(neighbours_values(bottom_right, map)).to match_array [9, 7]
+  end
+end
+
+RSpec.describe '#low_points_values' do
+  it 'finds the value of the low points on the map' do
+    expect(low_points_values(map)).to eq [1, 0, 5, 5]
   end
 end
 
 RSpec.describe '#low_points' do
-  it 'finds the low points on the map' do
-    input_map = map
+  it 'returns the low points values and their coordinates' do
+    expect(low_points(map)).to eq ({
+      "0:1" => 1, 
+      "0:9" => 0, 
+      "2:2" => 5, 
+      "4:6" => 5
+    })
+  end
+end
 
-    expect(low_points(input_map)).to eq [1, 0, 5, 5]
+RSpec.describe '#neighbours' do
+  it 'finds the neighbours of the specified location' do
+    coordinates = Coordinate.new(2, 3)
+    
+    expect(neighbours(coordinates, map)).to eq (
+      {
+        "2:2" => 5, 
+        "3:1" => 7, 
+        "3:3" => 7, 
+        "4:2" => 9 
+      }
+    )
+  end
+
+  it 'returns less than 4 neighbours if the element is on the border of the map' do
+    top_left = Coordinate.new(0, 0)
+    bottom_right = Coordinate.new(map.first.size - 1, map.size - 1)
+
+    expect(neighbours(top_left, map)).to eq ({"0:1" => 1, "1:0" => 3})
+    expect(neighbours(bottom_right, map)).to eq ({"3:9" => 9, "4:8" => 7})
   end
 end
 
