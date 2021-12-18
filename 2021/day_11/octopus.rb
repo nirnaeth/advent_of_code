442aeae9a3a6d2e45a84e45c581534a8dcb99c36
Monica Giambitto <m.giambitto@freegoweb.it>
2021-12-18 22:28:31 +0100

    2021 day 11 part 1 use recursion to flash neighbours

--- 2021/day_11/octopus.rb
+++ 2021/day_11/octopus.rb
@@ -32,32 +32,126 @@ LOWEST = 0
 HIGHEST = 9
 STEPS = 100
 
-def setup(coords)
-  [].tap do |ledger|
-    (0..9).each do |energy_level|
-      ledger[energy_level] = [] if ledger[energy_level].nil?
-      same_level = coords.select { |_, v| v == energy_level }
-      ledger[energy_level] << same_level if same_level.any?
-    end
-  end
+Coordinate = Struct.new(:x, :y)
+
+# def setup(coords)
+#   [].tap do |ledger|
+#     (0..9).each do |energy_level|
+#       ledger[energy_level] = [] if ledger[energy_level].nil?
+#       same_level = coords.select { |_, v| v == energy_level }
+#       ledger[energy_level] << same_level if same_level.any?
+#     end
+#   end
+# end
+
+def neighbours(coordinates, map)
+  neighbours = {}
+
+  neighbours["#{coordinates.y}:#{coordinates.x - 1}"] = map["#{coordinates.y}:#{coordinates.x - 1}"] # right
+  neighbours["#{coordinates.y + 1}:#{coordinates.x}"] = map["#{coordinates.y + 1}:#{coordinates.x}"] # down
+  neighbours["#{coordinates.y}:#{coordinates.x + 1}"] = map["#{coordinates.y}:#{coordinates.x + 1}"] # left
+  neighbours["#{coordinates.y - 1}:#{coordinates.x}"] = map["#{coordinates.y - 1}:#{coordinates.x}"] # top
+  
+  # diagonals - from [here](https://todd.ginsberg.com/post/advent-of-code/2021/day11/)
+
+  neighbours["#{coordinates.y - 1}:#{coordinates.x - 1}"] = map["#{coordinates.y - 1}:#{coordinates.x - 1}"] # right
+  neighbours["#{coordinates.y + 1}:#{coordinates.x + 1}"] = map["#{coordinates.y + 1}:#{coordinates.x + 1}"] # down
+  neighbours["#{coordinates.y - 1}:#{coordinates.x + 1}"] = map["#{coordinates.y - 1}:#{coordinates.x + 1}"] # left
+  neighbours["#{coordinates.y + 1}:#{coordinates.x - 1}"] = map["#{coordinates.y + 1}:#{coordinates.x - 1}"] # top
+
+  neighbours.select { |_, energy| energy != nil}
 end
+
+# ledger = setup(coordinates)
+
+# During a single step, the following occurs:
+
+# - First, the energy level of each octopus increases by 1.
+# - Then, any octopus with an energy level greater than 9 flashes. 
+#   This increases the energy level of all adjacent octopuses by 1, including octopuses that are diagonally adjacent. 
+#   If this causes an octopus to have an energy level greater than 9, it also flashes. 
+#   This process continues as long as new octopuses keep having their energy level increased beyond 9. 
+#   (An octopus can only flash at most once per step.)
+# - Finally, any octopus that flashed during this step has its energy level set to 0, as it used all of its energy to flash.
+
+def count_flashes(map, steps)
+  flashes = 0
 
 
 # octopi.each do |octupus|
 #   octopus += 1
-#   if octopus == 9
+#   if octopus > 9
 #     neighbours = all_neighbours += 1 # 8 around
 #     flash 
 #     record flashing
-#     octopus = 0
 
 #     neighbours.each do |neighbour|
 #       neighbours = all_neighbours += 1 # 8 around
 #       flash 
 #       record flashing
-#       octopus = 0
 #     end
 #   end
 # end
+
+  steps.times do |i|
+    # p "step #{i}"
+    map.transform_values! { |energy| energy += 1 }
+
+    # p map if i == 0
+
+    binding.pry 
+    
+    flashed = map.select { |_, energy| energy > 9 } # val flashersThisRound = cave.filterValues { it > 9 }.keys
+    # p "flashed naturally #{flashed.count}"
+
+    flashed.each do |coords, _| 
+      map[coords] = 0 # flashersThisRound.forEach { cave[it] = 0 }
+      flash(coords, map, flashed)
+
+      # p map
+      # p map.values.count  { |energy| energy == 0 }
+    end
+
+    flashes += map.values.count { |energy| energy == 0 }
+  end
+
+  flashes
+end
+
+# def flash(H, flashed): 
+#   flashing = [p for p, val in H.items() if val > 9 and p not in flashed]    
+#   if flashing: 
+#       for p in flashing: 
+#           H.update({q: H[q] + 1 for q in neighborhood(*p) if q in H.keys()}) 
+#       flash(H, flashing + flashed)
+
+def flash(coords, map, flashed)
+  # p "flashed"
+  # p flashed
+  binding.pry
+  y = coords.split(':').first.to_i
+  x = coords.split(':').last.to_i
+
+  octopus_neighbours = neighbours(Coordinate.new(x, y), map) # .flatMap { it.allNeighbors() }
+
+  to_flash = octopus_neighbours.select { |_, energy| energy > 0 } # filter cave[it] != 0
+  
+  binding.pry  
+  
+  to_flash.each do |coords, energy| 
+    map[coords] = energy += 1 # forEach { cave[it] = cave.getValue(it) + 1 }
+    if map[coords] > 9 && !flashed.keys.include?(coords)
+      map[coords] = 0 
+      flash(coords, map, flashed.merge({coords => energy}))
+    end
+  end
+end
+
+# p count_flashes(coordinates, 99)
+
+  # 4907 is too high
+  # 4869 is too high
+  # 1866 is too high
+  # 1846 in not too high nor too small (99)
 
 # how many flashes?
--- 2021/spec/day_11/octopus_spec.rb
+++ 2021/spec/day_11/octopus_spec.rb
@@ -27,26 +27,162 @@ RSpec.describe '#setup' do
     ])
   end
 end
-# RSpec.describe '#neighbours' do
-#   it 'finds the neighbours of the specified octopus' do
-#     # [2,  1 ,  9 ,  9 , 9, 4, 3, 2, 1, 0], 
-#     # [3,  9 ,  8 ,  7 , 8, 9, 4, 9, 2, 1], 
-#     # [9, _8_, _5_, _6_, 7, 8, 9, 8, 9, 2], 
-#     # [8, _7_, x6x, _7_, 8, 9, 6, 7, 8, 9], 
-#     # [9, _8_, _9_, _9_ , 9, 6, 5, 6, 7, 8]
-#     coordinates = Coordinate.new(2, 3) # x, y
+
+RSpec.describe '#update_levels' do
+  it 'increases the energy level of the octopi during one step' do
+    ledger = [
+      [], 
+      [{'0:3' => 1}], 
+      [{'0:4' => 2}], 
+      [{'0:0' => 3, '0:2' => 3}], 
+      [{'0:1' => 4}], 
+      [], 
+      [], 
+      [], 
+      [], 
+      []
+    ]
+
+    new_levels = update_levels(ledger)
+
+    expect(new_levels).to eq (
+      [
+        [], 
+        [], 
+        [{'0:3' => 1}], 
+        [{'0:4' => 2}], 
+        [{'0:0' => 3, '0:2' => 3}], 
+        [{'0:1' => 4}], 
+        [], 
+        [], 
+        [], 
+        []
+      ]
+    )
+  end
+
+  it 'resets an octopus energy level to 0 after a flash' do
+    ledger = [
+      [], 
+      [], 
+      [], 
+      [], 
+      [], 
+      [],
+      [{'0:3' => 1}], 
+      [{'0:4' => 2}], 
+      [{'0:0' => 3, '0:2' => 3}], 
+      [{'0:1' => 4}]
+    ]
+
+    new_levels = update_levels(ledger)
+
+    expect(new_levels).to eq (
+      [
+        [{'0:1' => 4}],
+        [], 
+        [], 
+        [], 
+        [], 
+        [], 
+        [],
+        [{'0:3' => 1}], 
+        [{'0:4' => 2}], 
+        [{'0:0' => 3, '0:2' => 3}]
+      ]
+    )
+  end
+end
+
+RSpec.describe '#neighbours' do
+  it 'finds the neighbours of the specified octopus' do
+    # [2,  1 ,  9 ,  9 , 9, 4, 3, 2, 1, 0], 
+    # [3,  9 ,  8 ,  7 , 8, 9, 4, 9, 2, 1], 
+    # [9, _8_, _5_, _6_, 7, 8, 9, 8, 9, 2], 
+    # [8, _7_, x6x, _7_, 8, 9, 6, 7, 8, 9], 
+    # [9, _8_, _9_, _9_ , 9, 6, 5, 6, 7, 8]
+    coordinates = Coordinate.new(2, 3) # x, y
     
-#     expect(neighbours(coordinates, map)).to eq (
-#       {
-#         "2:2" => 5, 
-#         "2:1" => 8, 
-#         "3:1" => 7,
-#         "4:1" => 8, 
-#         "4:2" => 9, 
-#         "4:3" => 9,
-#         "3:3" => 7,
-#         "2:3" => 6,
-#       }
-#     )
-#   end
-# end
+    expect(neighbours(coordinates, map)).to eq (
+      {
+        "2:2" => 5, 
+        "2:1" => 8, 
+        "3:1" => 7,
+        "4:1" => 8, 
+        "4:2" => 9, 
+        "4:3" => 9,
+        "3:3" => 7,
+        "2:3" => 6,
+      }
+    )
+  end
+
+  it 'discards neighbours out of the map' do
+    coordinates = Coordinate.new(1, 0) # x, y
+    octopi = {
+      '0:0' => 1,
+      '0:1' => 9,
+      '0:2' => 9,
+      '0:3' => 9,
+      '0:4' => 1
+    }
+
+    expect(neighbours(coordinates, octopi)).to eq (
+      {
+        "0:0" => 1, 
+        "0:2" => 9
+      }
+    )
+  end
+end
+
+RSpec.describe '#count_flashes' do
+  it 'increases energy levels by 1' do
+    octopi = {
+      '0:0' => 3,
+      '0:1' => 4,
+      '0:2' => 3,
+      '0:3' => 1,
+      '0:4' => 2
+    }
+
+    expect(count_flashes(octopi)).to eq (
+      {
+        '0:0' => 4,
+        '0:1' => 5,
+        '0:2' => 4,
+        '0:3' => 2,
+        '0:4' => 3
+      }
+    )
+  end
+
+  it 'counts the number of octopi that flash in a step when there are no flashing neighbours' do
+    octopi = {
+      '0:0' => 1, '0:1' => 1, '0:2' => 1, '0:3' => 1, '0:4' => 1,
+      '1:0' => 1, '1:1' => 9, '1:2' => 9, '1:3' => 9, '1:4' => 1,
+      '2:0' => 1, '2:1' => 9, '2:2' => 9, '2:3' => 9, '2:4' => 1,
+      '3:0' => 1, '3:1' => 9, '3:2' => 9, '3:3' => 9, '3:4' => 1,
+      '4:0' => 1, '4:1' => 1, '4:2' => 1, '4:3' => 1, '4:4' => 1
+    }
+
+    expect(count_flashes(octopi, 2)).to eq 9
+  end
+
+  it 'counts the number of octopi that flash in a step when there are flashing neighbours' do
+    path = 'spec/support/day_11/input.txt'
+    octopi = Input.new(path).to_coordinates
+
+    expect(count_flashes(octopi, 2)).to eq 35
+  end
+end
+
+def map
+  [
+    [2, 1, 9, 9, 9, 4, 3, 2, 1, 0], 
+    [3, 9, 8, 7, 8, 9, 4, 9, 2, 1], 
+    [9, 8, 5, 6, 7, 8, 9, 8, 9, 2], 
+    [8, 7, 6, 7, 8, 9, 6, 7, 8, 9], 
+    [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]
+  ]
+end
