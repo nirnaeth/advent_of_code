e72d4dbb5e67493653654cf5369f4052e1dc067e
Monica Giambitto <m.giambitto@freegoweb.it>
2021-12-19 12:25:50 +0100

    2021 day 11 part 1 fix neighbours test

--- 2021/spec/day_11/octopus_spec.rb
+++ 2021/spec/day_11/octopus_spec.rb
@@ -1,109 +1,13 @@
 require './day_11/octopus'
 require 'pry'
 
-RSpec.describe '#setup' do
-  it 'organizes octopi by energy level' do
-    octopi = {
-      '0:0' => 3,
-      '0:1' => 4,
-      '0:2' => 3,
-      '0:3' => 1,
-      '0:4' => 2
-    }
-    
-    ledger = setup(octopi)
-
-    expect(ledger).to eq ([
-      [], 
-      [{"0:3"=>1}], 
-      [{"0:4"=>2}], 
-      [{"0:0"=>3, "0:2"=>3}], 
-      [{"0:1"=>4}], 
-      [], 
-      [], 
-      [], 
-      [], 
-      []
-    ])
-  end
-end
-
-RSpec.describe '#update_levels' do
-  it 'increases the energy level of the octopi during one step' do
-    ledger = [
-      [], 
-      [{'0:3' => 1}], 
-      [{'0:4' => 2}], 
-      [{'0:0' => 3, '0:2' => 3}], 
-      [{'0:1' => 4}], 
-      [], 
-      [], 
-      [], 
-      [], 
-      []
-    ]
-
-    new_levels = update_levels(ledger)
-
-    expect(new_levels).to eq (
-      [
-        [], 
-        [], 
-        [{'0:3' => 1}], 
-        [{'0:4' => 2}], 
-        [{'0:0' => 3, '0:2' => 3}], 
-        [{'0:1' => 4}], 
-        [], 
-        [], 
-        [], 
-        []
-      ]
-    )
-  end
-
-  it 'resets an octopus energy level to 0 after a flash' do
-    ledger = [
-      [], 
-      [], 
-      [], 
-      [], 
-      [], 
-      [],
-      [{'0:3' => 1}], 
-      [{'0:4' => 2}], 
-      [{'0:0' => 3, '0:2' => 3}], 
-      [{'0:1' => 4}]
-    ]
-
-    new_levels = update_levels(ledger)
-
-    expect(new_levels).to eq (
-      [
-        [{'0:1' => 4}],
-        [], 
-        [], 
-        [], 
-        [], 
-        [], 
-        [],
-        [{'0:3' => 1}], 
-        [{'0:4' => 2}], 
-        [{'0:0' => 3, '0:2' => 3}]
-      ]
-    )
-  end
-end
 
 RSpec.describe '#neighbours' do
   it 'finds the neighbours of the specified octopus' do
-    # [2,  1 ,  9 ,  9 , 9, 4, 3, 2, 1, 0], 
-    # [3,  9 ,  8 ,  7 , 8, 9, 4, 9, 2, 1], 
-    # [9, _8_, _5_, _6_, 7, 8, 9, 8, 9, 2], 
-    # [8, _7_, x6x, _7_, 8, 9, 6, 7, 8, 9], 
-    # [9, _8_, _9_, _9_ , 9, 6, 5, 6, 7, 8]
-    coordinates = Coordinate.new(2, 3) # x, y
-    
-    expect(neighbours(coordinates, map)).to eq (
+    x = 2
+    y = 3    
+
+    expect(neighbours(x, y, coordinates)).to eq (
       {
         "2:2" => 5, 
         "2:1" => 8, 
@@ -115,10 +19,18 @@ RSpec.describe '#neighbours' do
         "2:3" => 6,
       }
     )
+
+    # [2,  1 ,  9 ,  9 , 9, 4, 3, 2, 1, 0], 
+    # [3,  9 ,  8 ,  7 , 8, 9, 4, 9, 2, 1], 
+    # [9, _8_, _5_, _6_, 7, 8, 9, 8, 9, 2], 
+    # [8, _7_, x6x, _7_, 8, 9, 6, 7, 8, 9], 
+    # [9, _8_, _9_, _9_ , 9, 6, 5, 6, 7, 8]
   end
 
   it 'discards neighbours out of the map' do
-    coordinates = Coordinate.new(1, 0) # x, y
+    x = 1
+    y = 0
+
     octopi = {
       '0:0' => 1,
       '0:1' => 9,
@@ -127,7 +39,7 @@ RSpec.describe '#neighbours' do
       '0:4' => 1
     }
 
-    expect(neighbours(coordinates, octopi)).to eq (
+    expect(neighbours(x, y, octopi)).to eq (
       {
         "0:0" => 1, 
         "0:2" => 9
@@ -136,27 +48,31 @@ RSpec.describe '#neighbours' do
   end
 end
 
-RSpec.describe '#count_flashes' do
-  it 'increases energy levels by 1' do
-    octopi = {
-      '0:0' => 3,
-      '0:1' => 4,
-      '0:2' => 3,
-      '0:3' => 1,
-      '0:4' => 2
-    }
-
-    expect(count_flashes(octopi)).to eq (
-      {
-        '0:0' => 4,
-        '0:1' => 5,
-        '0:2' => 4,
-        '0:3' => 2,
-        '0:4' => 3
+RSpec.describe '#step' do
+  context 'when there are no octopi ready to flash' do
+    it 'increases energy levels by 1' do
+      octopi = {
+        '0:0' => 3,
+        '0:1' => 4,
+        '0:2' => 3,
+        '0:3' => 1,
+        '0:4' => 2
       }
-    )
-  end
 
+      expect(count_flashes(octopi)).to eq (
+        {
+          '0:0' => 4,
+          '0:1' => 5,
+          '0:2' => 4,
+          '0:3' => 2,
+          '0:4' => 3
+        }
+      )
+    end
+  end
+end
+
+RSpec.describe '#count_flashes' do
   it 'counts the number of octopi that flash in a step when there are no flashing neighbours' do
     octopi = {
       '0:0' => 1, '0:1' => 1, '0:2' => 1, '0:3' => 1, '0:4' => 1,
@@ -175,6 +91,16 @@ RSpec.describe '#count_flashes' do
 
     expect(count_flashes(octopi, 10)).to eq 2
   end
+end
+
+def coordinates
+  {
+    '0:0' => 2, '0:1' => 1, '0:2' => 9, '0:3' => 9, '0:4' => 9, '0:5' => 4, '0:6' => 3, '0:7' => 2, '0:8' => 1, '0:9' => 0,
+    '1:0' => 3, '1:1' => 9, '1:2' => 8, '1:3' => 7, '1:4' => 8,' 1:5' => 9, '1:6' => 4, '1:7' => 9, '1:8' => 2, '1:9' => 1,
+    '2:0' => 9, '2:1' => 8, '2:2' => 5, '2:3' => 6, '2:4' => 7, '2:5' => 8, '2:6' => 9, '2:7' => 8, '2:8' => 9, '2:9' => 2,
+    '3:0' => 8, '3:1' => 7, '3:2' => 6, '3:3' => 7, '3:4' => 8, '3:5' => 9, '3:6' => 6, '3:7' => 7, '3:8' => 8, '3:9' => 9,
+    '4:0' => 9, '4:1' => 8, '4:2' => 9, '4:3' => 9, '4:4' => 9, '4:5' => 6, '4:6' => 5, '4:7' => 6, '4:8' => 7, '4:9' => 8,
+  }
 end
 
 def map
