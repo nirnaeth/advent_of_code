aab96be39f2650b1ff572cc4cb7861bd935e32f2
Monica Giambitto <m.giambitto@freegoweb.it>
2021-12-20 20:20:45 +0100

    2021 day 12 part 1 neighbours for each cave

--- 2021/day_12/cave_paths.rb
+++ 2021/day_12/cave_paths.rb
@@ -2,14 +2,12 @@
 
 $LOAD_PATH << './lib'
 require 'input'
+require_relative 'cave'
 require 'pry'
 
 path = 'support/day_12/input.txt'
 
-connections = Input.new(path).to_str_array
-
-START_POS = 'start'
-END_POS = 'end'
+map = Input.new(path).to_str_array
 
 # find the number of distinct paths that start at start, end at end and don't visit small caves more than once
 
@@ -17,3 +15,52 @@ END_POS = 'end'
 # all paths you find should visit small caves at most once, and can visit big caves any number of times
 
 # How many paths through this cave system are there that visit small caves at most once?
+
+# p connections
+
+# def list_paths(map)
+#   {}.tap do |paths|
+#     map.each do |connection|
+#       pair = connection.split('-')
+
+#       all = pair.zip(pair.reverse)
+    
+#       all.each do |from, to|
+#         unless to == 'start' #|| from == 'end'
+#           paths[from].nil? ? paths[from] = [to] : paths[from] << to 
+#         end
+#       end
+#     end
+#   end
+# end
+
+def list_caves_paths(map)
+  {}.tap do |neighbours|
+    map.each do |line|
+      from, to = line.split('-')
+    
+      neighbours[from].nil? ? neighbours[from] = [to] : neighbours[from] << to
+    end
+  end
+end
+
+# paths = list_caves_paths(map)
+
+# def part1(paths, visited)
+#   final = 0
+
+#   paths[visited.last].each do |cave|
+#     # binding.pry
+#     if cave.upcase == cave || !visited.include?(cave)
+#       if cave == 'end'
+#         final += 1  
+#       else 
+#         part1(paths, visited << cave)
+#       end
+#     end
+#   end
+
+#   final
+# end
+
+#   p part1(paths, ['start'])
--- 2021/spec/day_12/cave_paths_spec.rb
+++ 2021/spec/day_12/cave_paths_spec.rb
@@ -1,4 +1,17 @@
 require './day_12/cave_paths'
 require 'pry'
 
-# How many paths through this cave system are there that visit small caves at most once?
+RSpec.describe '#list_caves_paths' do
+  it 'lists all caves that can be visited from a cave' do
+    input = ['start-A', 'start-b', 'A-c', 'A-b', 'b-d', 'A-end', 'b-end']
+    input = ['start-A', 'start-b', 'A-c', 'A-b', 'b-d', 'A-end', 'b-end']
+
+    paths = list_caves_paths(input)
+
+    expect(list_caves_paths(input)).to match ({
+      'start' => ['A', 'b'],
+      'A' => ['c', 'b', 'end'],
+      'b' => ['d', 'end']
+    })
+  end
+end
