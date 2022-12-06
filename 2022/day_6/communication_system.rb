b0ace5a6768c11eb70710c7c464d76c20db5873e
Monica Giambitto <m.giambitto@freegoweb.it>
2022-12-06 15:08:49 +0100

    2022 day 6 part 1 solution

--- 2022/day_6/communication_system.rb
+++ 2022/day_6/communication_system.rb
@@ -3,9 +3,16 @@
 $LOAD_PATH << './lib'
 require "input.rb"
 path = "data/day_6/input.txt"
-transmission = Input.new(path).to_str_array
+transmission = Input.new(path).to_chr_array
 
 def packet_start?(sequence)
-  current = sequence.shift
-  !sequence.include? current
+  sequence.count == sequence.uniq.count
 end
+
+def marker_position(transmission)
+  transmission.each_index do |index|
+    return index + 4 if packet_start?(transmission[index, 4])
+  end
+end
+
+p marker_position(transmission)
--- 2022/spec/day_6/communication_system_spec.rb
+++ 2022/spec/day_6/communication_system_spec.rb
@@ -2,23 +2,24 @@ require "pry"
 require "./day_6/communication_system.rb"
 
 RSpec.describe "#packet_start?" do
-  it "returns true if the packet start marker is reached" do
-    test_data = ["j", "p", "q", "m"]
+  it "returns true if there is a sequence of 4 non repeating characters" do
+    test_data = ["m", "j", "q", "p"]
 
     expect(packet_start?(test_data)).to be true
   end
+  
+  it "returns false if there are duplicate characters in a sequence of 4" do
+    test_data = ["m", "j", "q", "j"]
+
+    expect(packet_start?(test_data)).to be false
+  end
 end
 
-RSpec.describe "#move" do
-  xit "returns true if the packet start marker is reached" do
-    path = "spec/data/day_5/input.txt"
-    _, test_data = Input.new(path).to_str_array("\n\n")
+RSpec.describe "#marker_position" do
+  it "returns the number of characters to be processed before the packet start marker is identified" do
+    path = "spec/data/day_6/input.txt"
+    test_data = Input.new(path).to_chr_array
 
-    expect(moves(test_data)).to match_array [
-      { moves: 1, from: 2, to: 1 },
-      { moves: 3, from: 1, to: 3 },
-      { moves: 2, from: 2, to: 1 },
-      { moves: 1, from: 1, to: 2 }
-    ]
+    expect(marker_position(test_data)).to eq 7
   end
 end
