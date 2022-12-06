543c4cabfd103c3c57aa65ed3f7b7bca4f984b42
Monica Giambitto <m.giambitto@freegoweb.it>
2022-12-06 16:33:10 +0100

    2020 day 5 part 1 solution

--- 2020/day_5/sit.rb
+++ 2020/day_5/sit.rb
@@ -5,3 +5,67 @@ require "input.rb"
 path = "support/input_day_5.txt"
 
 boarding_passes = Input.new(path).to_str_array
+
+def parse(boarding_pass)
+  rows = boarding_pass[0, 7] 
+  columns = boarding_pass[7, 3]
+  return rows, columns
+end
+
+def row(sequence, min: 0, max: 127)
+  sequence.each_char do |c|
+    case c
+    when "F"
+      min = min
+      max -= ((max - min) / 2.0).ceil
+    when "B"      
+      min += ((max - min) / 2.0).ceil
+      max = max
+    end
+
+    sequence.delete_prefix!(c)
+    if min == max
+      return max
+    else
+      row(sequence, min: min, max: max)
+    end
+  end
+end
+
+def column(sequence, min: 0, max: 7)
+  sequence.each_char do |c|
+    case c
+    when "L"
+      min = min
+      max -= ((max - min) / 2.0).ceil
+    when "R"      
+      min += ((max - min) / 2.0).ceil
+      max = max
+    end
+
+    sequence.delete_prefix!(c)
+    if min == max
+      return max
+    else
+      row(sequence, min: min, max: max)
+    end
+  end
+end
+
+def seat(row, column)
+  row * 8 + column
+end
+
+def highest_seat(passes)
+  [].tap do |a|
+    passes.each do |pass|
+      rows, columns = parse(pass)
+      row_number = row(rows)
+      column_number = column(columns)
+      
+      a << seat(row_number, column_number)
+    end
+  end.max
+end
+
+p highest_seat(boarding_passes)
--- 2020/spec/day_5/sit_spec.rb
+++ 2020/spec/day_5/sit_spec.rb
@@ -1,11 +1,44 @@
 require "pry"
 require "./day_5/sit.rb"
 
-RSpec.describe "#overlaps" do
-  xit "counts how many assignments are overlapping" do
+RSpec.describe "#parse" do
+  it "returns the sequences for rows and columns from the boarding pass" do
+    test_data = "FBFBBFFRLR"
+
+    expect(parse(test_data)).to match_array ["FBFBBFF", "RLR"]
+  end
+end
+
+RSpec.describe "#row" do
+  it "identifies the row where the seat is" do
+    test_data = "FBFBBFF"
+
+    expect(row(test_data)).to eq 44
+  end
+end
+
+RSpec.describe "#column" do
+  it "identifies the row where the seat is" do
+    test_data = "RLR"
+
+    expect(column(test_data)).to eq 5
+  end
+end
+
+RSpec.describe "#seat" do
+  it "identifies the seat" do
+    row = 44
+    column = 5
+
+    expect(seat(row, column)).to eq 357
+  end
+end
+
+RSpec.describe "highest_seat" do
+  it "returns the seat with the highest ID" do
     path = "spec/support/input_day_5.txt"
     test_data = Input.new(path).to_str_array
 
-    expect(overlaps(test_data)).to eq 2
+    expect(highest_seat(test_data)).to eq 820
   end
 end
