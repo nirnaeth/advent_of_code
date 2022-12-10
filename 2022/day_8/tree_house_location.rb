154c05fbf1048a312ac61b6a184b6a237da32c1d
Monica Giambitto <m.giambitto@freegoweb.it>
2022-12-11 00:25:47 +0100

    2022 day 8 part 1 tree visibility

--- 2022/day_8/tree_house_location.rb
+++ 2022/day_8/tree_house_location.rb
@@ -8,17 +8,61 @@ map = Input.new(path).to_coordinates
 Tree = Struct.new('Tree', :x, :y, :height)
 
 def visible?(map, tree)
-  y = tree[0]
-  x = tree[1]
-  
-  edges?(map, x, y) 
+  edges?(map, tree) || line_of_sight?(map, tree)
 end
 
 private
 
-  def edges?(map, x, y)
-    return true if x == 0 || y == 0
+  def edges?(map, tree)
+    return true if tree.x == 0 || tree.y == 0
 
-    max_y, max_x = map.keys.last.split(":").map(&:to_i)
-    return true if y == max_y || x == max_x
+    max_x, max_y = map.keys.last.split(":").map(&:to_i)
+    return true if tree.x == max_x ||  tree.y == max_y
+  end
+
+  def top_max(map, tree)
+    x = tree.x
+    y = tree.y
+
+    [].tap do |a|
+      (0..y - 1).each { |n| a << map["#{x}:#{n}"] }
+    end.max
+  end
+
+  def right_max(map, tree)
+    x = tree.x
+    y = tree.y
+
+    max_x, max_y = map.keys.last.split(":").map(&:to_i)
+    
+    [].tap do |a|
+      (x + 1..max_x).each { |n| a << map["#{n}:#{y}"] }
+    end.max
+  end
+
+  def down_max(map, tree)
+    x = tree.x
+    y = tree.y
+
+    max_x, max_y = map.keys.last.split(":").map(&:to_i)
+
+    [].tap do |a|
+      (y + 1..max_y).each { |n| a << map["#{x}:#{n}"] }
+    end.max
+  end
+
+  def left_max(map, tree)
+    x = tree.x
+    y = tree.y
+    
+    [].tap do |a|
+      (0..x - 1).each { |n| a << map["#{n}:#{y}"] }
+    end.max
+  end
+
+  def line_of_sight?(map, tree)
+    top_max(map, tree) < tree.height || 
+    right_max(map, tree) < tree.height || 
+    down_max(map, tree) < tree.height || 
+    left_max(map, tree) < tree.height
   end
--- 2022/spec/day_8/tree_house_location_spec.rb
+++ 2022/spec/day_8/tree_house_location_spec.rb
@@ -6,10 +6,44 @@ RSpec.describe "#visible?" do
     path = "spec/data/day_8/input.txt"
     test_data = Input.new(path).to_coordinates
 
-    first_tree = [0, 0]
-    last_tree =  [4, 4]
+    first_tree = Tree.new(0, 0, test_data["0:0"])
+    last_tree = Tree.new(4, 4, test_data["4:4"])
 
     expect(visible?(test_data, first_tree)).to eq true
     expect(visible?(test_data, last_tree)).to eq true
+  end
+
+  it "returns true when the tree has line of sight from at least one of the edges" do
+    test_data = {
+      "0:0"=>3, "1:0"=>0, "2:0"=>3, "3:0"=>7, "4:0"=>3,
+      "0:1"=>5, "1:1"=>5, "2:1"=>5, "3:1"=>4, "4:1"=>2,
+      "0:2"=>6, "1:2"=>5, "2:2"=>3, "3:2"=>3, "4:2"=>2,
+      "0:3"=>3, "1:3"=>3, "2:3"=>5, "3:3"=>1, "4:3"=>9,
+      "0:4"=>3, "1:4"=>5, "2:4"=>5, "3:4"=>0, "4:4"=>0
+    }
+
+    # right
+    visible_tree = Tree.new(1, 2, test_data["1:2"])
+    expect(visible?(test_data, visible_tree)).to eq true
+
+    # left
+    visible_tree = Tree.new(2, 3, test_data["2:3"])
+    expect(visible?(test_data, visible_tree)).to eq true
+
+    # top
+    visible_tree = Tree.new(1, 1, test_data["1:1"])
+    expect(visible?(test_data, visible_tree)).to eq true
+
+    # down
+    visible_tree = Tree.new(3, 1, test_data["3:1"])
+    expect(visible?(test_data, visible_tree)).to eq true
+  end
+
+  it "returns false when the tree has no line of sight" do
+    path = "spec/data/day_8/input.txt"
+    test_data = Input.new(path).to_coordinates
+
+    visible_tree = Tree.new(1, 3, test_data["1:3"])
+    expect(visible?(test_data, visible_tree)).to eq false
   end
 end
