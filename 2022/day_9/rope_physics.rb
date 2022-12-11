c4c3af9298c17ac1365716dee2658d7b910bc0e8
Monica Giambitto <m.giambitto@freegoweb.it>
2022-12-11 21:42:31 +0100

    2022 day 9 part 1 move head and tail to right

--- 2022/day_9/rope_physics.rb
+++ 2022/day_9/rope_physics.rb
@@ -8,18 +8,31 @@ instructions = Input.new(path).to_str_array
 Point = Struct.new("Point", :x, :y,)
 
 def touching?(head, tail)
-  (head.x - tail.x).abs <= 1 ||
-  (head.y - tail.y).abs <= 1
+  (head.x - tail.x).abs <= 1 &&
+  (head.y - tail.y).abs <= 1 
 end
 
-def move(instruction, head)
+def move(instruction, head, tail)
   direction, steps = instruction.split(" ")
   steps = steps.to_i
     
   case direction
   when "R" 
-    [head.x + steps, head.y]
+    head.x += steps
+    
+    if !touching?(head, tail) 
+      if head.x != tail.x && head.y == tail.y # same row, different column
+        tail.x = head.x - 1
+      end
+    end
+    
+    { 
+      head: [head.x, head.y],
+      tail: [tail.x, tail.y]
+    }
   when "U"
+    head.y += steps
+
     [head.x, head.y + steps]
   when "L"
     [head.x - steps, head.y]
--- 2022/spec/day_9/rope_physics_spec.rb
+++ 2022/spec/day_9/rope_physics_spec.rb
@@ -38,7 +38,16 @@ RSpec.describe "#touching?" do
     expect(touching?(head, tail)).to be true
   end
 
-  it "returns false if head and tail are further than 1 space away" do
+  it "returns false if head and tail are further than 1 space away on the same row" do
+    #... 
+    #H.T
+    head = Point.new(0, 0)
+    tail = Point.new(2, 0)
+
+    expect(touching?(head, tail)).to be false
+  end
+
+  it "returns false if head and tail are further than 1 space away on the diagonal" do
     #..T 
     #H..
     head = Point.new(0, 0)
@@ -46,21 +55,39 @@ RSpec.describe "#touching?" do
 
     expect(touching?(head, tail)).to be false
   end
+  
+  it "returns false if head and tail are further than 1 space away on the same column" do
+    #T.
+    #.. 
+    #H.
+    head = Point.new(0, 0)
+    tail = Point.new(0, 3)
+
+    expect(touching?(head, tail)).to be false
+  end
 end
 
 RSpec.describe "#move" do
-  it "moves the head right" do
+  it "moves the head right and the tail along the head when on the same row" do
     head = Point.new(0, 0)
+    tail = Point.new(0, 0)
     instruction = "R 4"
 
-    expect(move(instruction, head)).to eq [4, 0]
+    expect(move(instruction, head, tail)).to eq({
+      head: [4, 0],
+      tail: [3, 0]
+    })
   end
 
-  it "moves the head up" do
-    head = Point.new(0, 0)
+  it "moves the head up and the tail along the head when on the same column" do
+    head = Point.new(4, 0)
+    tail = Point.new(3, 0)
     instruction = "U 4"
 
-    expect(move(instruction, head)).to eq [0, 4]
+    expect(move(instruction, head, tail)).to eq({
+      head: [4, 4],
+      tail: [4, 3]
+    })
   end
 
   it "moves the head left" do
