70e418bb5b079e1138ddb2cce98155dd2cccce50
Monica Giambitto <monica.giambitto@pm.me>
2024-12-15 15:25:28 +0100

    2024 day 1 - calculate similarity between lists

--- 2024/day_1/distance_1.rb
+++ 2024/day_1/distance_1.rb
@@ -11,16 +11,23 @@ def lists(distances)
     .map(&:sort)
 end
 
-# p max(stashes)
-
 def calculate_distance(left_list, right_list)
-  distance = 0
-  left_list.size.times do |n|
-    distance += (left_list[n] - right_list[n]).abs
-  end
-
-  distance
+  [].tap do |a|
+    left_list.each_with_index do |_, index|
+      a << (left_list[index] - right_list[index]).abs
+    end
+  end.sum
 end
 
 inputs = lists(distances)
-p calculate_distance(inputs.first, inputs.last)
+# p calculate_distance(inputs.first, inputs.last)
+
+def calculate_similarity(left_list, right_list)
+  [].tap do |a|
+    left_list.each do |n|
+      a << (n * right_list.count(n))
+    end
+  end.sum
+end
+
+p calculate_similarity(inputs.first, inputs.last)
--- 2024/spec/day_1/distance_1_spec.rb
+++ 2024/spec/day_1/distance_1_spec.rb
@@ -14,12 +14,18 @@ end
 
 RSpec.describe "#calculate_distance" do
   it "returns the distance between two lists of locations" do
-    # path = "spec/day_1/data/input.txt"
-    # test_data = Input.new(path).to_str_array("\n\n")
-
     left_list = [1, 2, 3, 3, 3, 4]
     right_list = [3, 3, 3, 4, 5, 9]
 
     expect(calculate_distance(left_list, right_list)).to eq 11
+  end
+end
+
+RSpec.describe "#calculate_similarity" do
+  it "returns the distance between two lists of locations" do
+    left_list = [1, 2, 3, 3, 3, 4]
+    right_list = [3, 3, 3, 4, 5, 9]
+
+    expect(calculate_similarity(left_list, right_list)).to eq 31
   end
 end
