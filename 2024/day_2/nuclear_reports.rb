afbaf43008aa61d7fb56245e12c8403368bbac66
Monica Giambitto <monica.giambitto@pm.me>
2024-12-15 15:48:18 +0100

    2024 Day 2 - basic unsafe conditions

--- 2024/day_2/nuclear_reports.rb
+++ 2024/day_2/nuclear_reports.rb
@@ -3,4 +3,9 @@
 $LOAD_PATH << './lib'
 require "input.rb"
 path = "day_2/data/input.txt"
-reports = Input.new(path).to_pairs_array("\n")
+reports = Input.new(path).to_int_matrix("\n")
+
+def is_safe?(report)
+  # this fails no matter what when the array is decreasing
+  return false if report != report.sort
+end
--- 2024/spec/day_2/nuclear_reports_spec.rb
+++ 2024/spec/day_2/nuclear_reports_spec.rb
@@ -1,31 +1,16 @@
 require "pry"
 require "./day_2/nuclear_reports.rb"
 
-RSpec.describe "#lists" do
-  it "prepares the lists" do
-    input = [[3, 4], [4, 3], [2, 5], [1, 3], [3, 9], [3, 3]]
+RSpec.describe "#is_safe?" do
+  it "returns false if the report is not all increasing or all decreasing" do
+    report = [1, 3, 2, 4, 5]
 
-    left_list = [1, 2, 3, 3, 3, 4]
-    right_list = [3, 3, 3, 4, 5, 9]
-
-    expect(lists(input)).to eq [left_list, right_list]
+    expect(is_safe?(report)).to be false
   end
-end
 
-RSpec.describe "#calculate_distance" do
-  it "returns the distance between two lists of locations" do
-    left_list = [1, 2, 3, 3, 3, 4]
-    right_list = [3, 3, 3, 4, 5, 9]
+  it "returns false if the report is not all increasing or all decreasing" do
+    report = [8, 6, 4, 4, 1]
 
-    expect(calculate_distance(left_list, right_list)).to eq 11
-  end
-end
-
-RSpec.describe "#calculate_similarity" do
-  it "returns the distance between two lists of locations" do
-    left_list = [1, 2, 3, 3, 3, 4]
-    right_list = [3, 3, 3, 4, 5, 9]
-
-    expect(calculate_similarity(left_list, right_list)).to eq 31
+    expect(is_safe?(report)).to be false
   end
 end
